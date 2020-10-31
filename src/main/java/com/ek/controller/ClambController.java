8d73df7ccd0c22cbaa0f7e06a13b5cd541511708
EricKuan <b9413048@gmail.com>
2020-10-31 16:23:30 +0800

    modify: 修正 javaScript 的執行時間，減少系統負擔，修正取得所有頁數的問題

--- src/main/java/com/ek/controller/ClambController.java
+++ src/main/java/com/ek/controller/ClambController.java
@@ -1,73 +1,64 @@
 package com.ek.controller;
 
+import com.ek.executor.GetPagePhotoExecutor;
+import com.ek.service.ClamberService;
+import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.Scanner;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
-import java.util.stream.Collectors;
-
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.ek.configuration.SystemConfig;
-import com.ek.executor.GetPagePhotoExecutor;
-import com.ek.service.ClamberService;
-import com.gargoylesoftware.htmlunit.BrowserVersion;
-import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
-import com.gargoylesoftware.htmlunit.WebClient;
-import com.gargoylesoftware.htmlunit.html.HtmlElement;
-import com.gargoylesoftware.htmlunit.html.HtmlPage;
-
 
 @Service
 public class ClambController {
 
-	
-	@Autowired
-	ClamberService clambService;
-	
-	@Autowired
-	SystemConfig conf;
+  @Autowired ClamberService clambService;
 
-	private static final Logger logger = LogManager.getLogger(GetPagePhotoExecutor.class);
 
-	public void exec() throws Exception {
-		
-		ExecutorService executor = Executors.newFixedThreadPool(5);
-		FileInputStream fileInStreamObj = new FileInputStream("./conf/downliadList.txt");
-		InputStream inStreamObject = fileInStreamObj;
-		Scanner sc = new Scanner(inStreamObject);
-		ArrayList<CompletableFuture> result = new ArrayList<>();
-		while (sc.hasNext()) {
-			String url = sc.nextLine();
-			
-			result.add(CompletableFuture.runAsync(()->{
-				try {
-					clambService.exec(url);
-				} catch (FailingHttpStatusCodeException | IOException | InterruptedException | ExecutionException e) {
-					logger.error(e);
-				}
-			}, executor));
-			
-		}
-		
-		CompletableFuture[] jobs = new CompletableFuture[result.size()];
-		result.toArray(jobs);
-		CompletableFuture future = CompletableFuture.allOf(jobs);
-		future.get();
-		System.out.println("Mission Completed");
-		executor.shutdown();
-	}
+  private static final Logger logger = LogManager.getLogger(GetPagePhotoExecutor.class);
+
+  public void exec() throws Exception {
+
+    FileInputStream fileInStreamObj = new FileInputStream("./conf/downliadList.txt");
+    InputStream inStreamObject = fileInStreamObj;
+    Scanner sc = new Scanner(inStreamObject);
+    ArrayList<CompletableFuture> result = new ArrayList<>();
+
+    ExecutorService executor = Executors.newFixedThreadPool(5);
+    try {
+      while (sc.hasNext()) {
+        String url = sc.nextLine();
+
+        result.add(
+            CompletableFuture.runAsync(
+                () -> {
+                  try {
+                    clambService.exec(url);
+                  } catch (FailingHttpStatusCodeException e) {
+                    logger.error(e);
+                    e.printStackTrace();
+                  }
+                },
+                executor));
+      }
+
+      CompletableFuture[] jobs = new CompletableFuture[result.size()];
+      result.toArray(jobs);
+      CompletableFuture future = CompletableFuture.allOf(jobs);
+      future.join();
+      System.out.println("ClambController Completed");
+    } catch (Exception e) {
+      logger.error(e);
+      e.printStackTrace();
+    } finally {
+      executor.shutdown();
+    }
+  }
 }
--- src/main/java/com/ek/service/ClamberService.java
+++ src/main/java/com/ek/service/ClamberService.java
@@ -8,6 +8,9 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import com.gargoylesoftware.htmlunit.BrowserVersion;
+import com.gargoylesoftware.htmlunit.ScriptPreProcessor;
+import net.sourceforge.htmlunit.corejs.javascript.EvaluatorException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.jsoup.Jsoup;
@@ -25,57 +28,94 @@ import com.gargoylesoftware.htmlunit.html.HtmlPage;
 @Service
 public class ClamberService {
 
-	private static final Logger logger = LogManager.getLogger(ClamberService.class);
-	private static String BASE_URL = "https://ahri-hentai.com/readOnline2.php?ID=";
-	
-	@Autowired
-	SystemConfig conf;
-	
-	public void exec(String url) throws FailingHttpStatusCodeException, MalformedURLException, IOException, InterruptedException, ExecutionException {
-		WebClient webClient = new WebClient();
+  private static final Logger logger = LogManager.getLogger(ClamberService.class);
+  private static String BASE_URL = "https://ahri-hentai.com/readOnline2.php?ID=";
 
-		String ID = url.substring(url.indexOf("ID=") + 3, url.length());
-		logger.info(ID);
+  @Autowired SystemConfig conf;
 
-		webClient.getOptions().setUseInsecureSSL(false);
-		webClient.getOptions().setJavaScriptEnabled(true);
-		webClient.getOptions().setCssEnabled(false);
-		webClient.getOptions().setRedirectEnabled(true);
-		webClient.getOptions().setThrowExceptionOnScriptError(false);
-		webClient.getOptions().setTimeout(5000);
-		HtmlPage htmlPage = webClient.getPage(BASE_URL + ID + "&host_id=0");
-		webClient.waitForBackgroundJavaScript(3000);
-		HtmlElement body = htmlPage.getBody();
-//		logger.info(body.asXml());
-		Document doc = Jsoup.parse(body.asXml());
+  public void exec(String url) {
+    try {
 
-		String pages = doc.getElementById("next_page_btn_area2").select("a").last().attr("href");
-		pages = pages.substring(pages.lastIndexOf("=") + 1, pages.length());
-//		logger.info(pages);
+      String ID = url.substring(url.indexOf("ID=") + 3, url.length());
+      logger.info(ID);
+      HtmlPage htmlPage;
+      int maxPage=0;
+      try (WebClient webClient = getWebClient()) {
+        htmlPage = webClient.getPage(BASE_URL + ID + "&host_id=0");
+        webClient.waitForBackgroundJavaScript(1000);
+        HtmlElement body = htmlPage.getBody();
+        		logger.info(body.asXml());
+        Document doc = Jsoup.parse(body.asXml());
 
-		int maxPage = Integer.valueOf(pages);
+        String pages = doc.getElementById("next_page_btn_area2").select("a").last().attr("href");
+        logger.info(pages);
+        pages = pages.split("page=")[1].split("&")[0];
+//        pages = pages.substring(pages.lastIndexOf("=") + 1, pages.length());
 
-		ArrayList<CompletableFuture> result = new ArrayList<>();
-		ExecutorService executor = Executors.newFixedThreadPool(5);
-		for (int i = 1; i <= maxPage; i += 2) {
-			htmlPage = webClient.getPage(BASE_URL + ID + "&host_id=17&page=" + i);
-			webClient.waitForBackgroundJavaScript(3000);
-			HtmlElement pageData = htmlPage.getBody();
-			GetPagePhotoExecutor page = new GetPagePhotoExecutor();
-			page.setTitle(htmlPage.getTitleText());
-			page.setPageNumber(i);
-			page.setPageData(pageData);
-			page.setSavePath(conf.savepath);
+        logger.info(pages);
 
-			result.add(CompletableFuture.runAsync(page, executor));
+        maxPage = Integer.valueOf(pages);
+      }
 
-		}
-		webClient.close();
-		CompletableFuture[] jobs = new CompletableFuture[result.size()];
-		result.toArray(jobs);
-		CompletableFuture future = CompletableFuture.allOf(jobs);
-		future.get();
-		System.out.println("Mission Completed");
-		executor.shutdown();
-	}
+      ArrayList<CompletableFuture> result = new ArrayList<>();
+
+      ExecutorService executor = Executors.newFixedThreadPool(5);
+      try {
+        for (int i = 1; i <= maxPage; i += 2) {
+          try (WebClient pageClient = getWebClient()) {
+
+            htmlPage = pageClient.getPage(BASE_URL + ID + "&host_id=17&page=" + i);
+            pageClient.waitForBackgroundJavaScript(1000);
+            HtmlElement pageData = htmlPage.getBody();
+            GetPagePhotoExecutor page = new GetPagePhotoExecutor();
+            page.setTitle(htmlPage.getTitleText());
+            page.setPageNumber(i);
+            page.setPageData(pageData);
+            page.setSavePath(conf.savepath);
+
+            result.add(CompletableFuture.runAsync(page, executor));
+          }
+        }
+        CompletableFuture[] jobs = new CompletableFuture[result.size()];
+        result.toArray(jobs);
+        CompletableFuture future = CompletableFuture.allOf(jobs);
+        future.get();
+        System.out.println("ClamberService Completed");
+        executor.shutdown();
+      } catch (EvaluatorException e) {
+        // Do nothing
+      } finally {
+        executor.shutdown();
+      }
+    } catch (Exception e) {
+      logger.error(e);
+      e.printStackTrace();
+    }
+  }
+
+  private WebClient getWebClient() {
+    WebClient webClient = new WebClient();
+    webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
+    webClient.getOptions().setUseInsecureSSL(false);
+    webClient.getOptions().setJavaScriptEnabled(true);
+    webClient.getOptions().setCssEnabled(false);
+    webClient.getOptions().setDownloadImages(false);
+
+    webClient.getOptions().setPopupBlockerEnabled(false);
+    webClient.setScriptPreProcessor(
+        new ScriptPreProcessor() {
+          @Override
+          public String preProcess(
+              HtmlPage htmlPage, String s, String s1, int i, HtmlElement htmlElement) {
+            return s.replaceAll("<http*ads*>","");
+//            return s;
+          }
+        });
+    webClient.getOptions().setRedirectEnabled(false);
+    webClient.getOptions().setThrowExceptionOnScriptError(false);
+    webClient.getOptions().setTimeout(3000);
+    webClient.setJavaScriptTimeout(300);
+
+    return webClient;
+  }
 }
